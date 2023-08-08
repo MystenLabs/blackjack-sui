859589edb1e73f5bfc420a930b84c14e94d8b093
Theodore Chaikalis <teo@mystenlabs.com>
2023-08-08 18:38:55 +0300

    added sequence diagram

--- app/src/app/components/navbar/Navbar.tsx
+++ app/src/app/components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
     <div className="flex justify-between items-center p-[8px] h-[60px] border-b-gray-400 border-b-[1px] sticky top-0">
       <div className="flex justify-start items-center gap-[14px]">
           <h1 className="text-3xl font-normal mb-3">
-              Blackjack on <Image src={suiLogo} alt="Sui" className="inline w-7"/> </h1>
+              <Image src={suiLogo} alt="Sui" className="inline w-7"/> Blackjack </h1>
       </div>
       <ConnectButton />
     </div>
