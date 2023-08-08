95052033680d958170293f4d74d3f77cbbec59b3
Theodore Chaikalis <teo@mystenlabs.com>
2023-08-08 15:29:36 +0300

    added sui logo on navbar

--- app/src/app/components/navbar/Navbar.tsx
+++ app/src/app/components/navbar/Navbar.tsx
@@ -1,30 +1,17 @@
 "use client";
 
-import Link from "next/link";
 import { ConnectButton } from "@mysten/wallet-kit";
-import { usePathname, useRouter } from "next/navigation";
-import { useGetNavigations } from "@/app/hooks/useGetNavigations";
 import React from "react";
+import suiLogo from "@/app/assets/sui-logo.png";
+import Image from 'next/image';
 
 export const Navbar = () => {
-  // const pathname = usePathname();
-  // const { navigations } = useGetNavigations();
 
   return (
     <div className="flex justify-between items-center p-[8px] h-[60px] border-b-gray-400 border-b-[1px] sticky top-0">
       <div className="flex justify-start items-center gap-[14px]">
-          <h1 className="text-3xl font-bold mb-3">Sui Blackjack!</h1>
-        {/*{navigations.map(({ title, href }) => (*/}
-        {/*  <Link*/}
-        {/*    key={href}*/}
-        {/*    className={`text-lg font-weight-500 ${*/}
-        {/*      pathname === href ? "underline" : ""*/}
-        {/*    }`}*/}
-        {/*    href={href}*/}
-        {/*  >*/}
-        {/*    {title}*/}
-        {/*  </Link>*/}
-        {/*))}*/}
+          <h1 className="text-3xl font-normal mb-3">
+              Blackjack on <Image src={suiLogo} alt="Sui" className="inline w-7"/> </h1>
       </div>
       <ConnectButton />
     </div>
