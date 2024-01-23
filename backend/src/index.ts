5ba52a3384c5f3f62b706848dc073a7e1c63547b
atha <giannisj3@gmail.com>
2024-01-23 12:05:44 +0200

    Add initial socket.io config to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,28 @@
 import express, { Request, Response } from "express";
 import helmet from "helmet";
 import cors from "cors";
+import { createServer } from "http";
 import { PORT } from "./utils/config";
 import { StatusCodes } from "http-status-codes";
 import { logger } from "./utils/logger";
 import bodyParser from "body-parser";
+import { Server } from "socket.io";
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.get("/", (req: Request, res: Response) => {
   logger.info("GET: /");
@@ -19,6 +31,13 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 
-app.listen(PORT || 3000, () => {
+io.on("connection", (socket) => {
+  logger.info("A user connected");
+  socket.on("disconnect", () => {
+    logger.info("A user disconnected");
+  });
+});
+
+server.listen(PORT || 3000, () => {
   logger.info(`Server is running on port ${PORT || 3000}`);
 });
