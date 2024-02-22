e3dc73be90af7259b82c30a8f013887cfd85ba96
atha <giannisj3@gmail.com>
2024-02-22 15:42:47 +0200

    Polish UI

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -122,19 +122,21 @@ const HomePage = () => {
         )}
         {game.status !== 0 && (
           <div className="flex flex-col items-center space-y-[20px]">
-            <div className="text-gray-100">The game is finished!</div>
-            {game.status === 3 && (
-              <div className="text-gray-300">It&apos;s a tie!</div>
-            )}
-            <div className="flex space-x-1 items-center">
-              <div className="text-gray-100 text-sm">
-                Object on Sui Explorer:
+            <div className="flex flex-col items-center space-y-[5px]">
+              <div className="text-gray-100">The game is finished!</div>
+              {game.status === 3 && (
+                <div className="text-gray-300">It&apos;s a tie!</div>
+              )}
+              <div className="flex space-x-1 items-center">
+                <div className="text-gray-100 text-sm">
+                  Object on Sui Explorer:
+                </div>
+                <SuiExplorerLink
+                  objectId={game.id.id}
+                  type="object"
+                  className="!text-gray-300 text-sm"
+                />
               </div>
-              <SuiExplorerLink
-                objectId={game.id.id}
-                type="object"
-                className="text-gray-300 text-sm"
-              />
             </div>
             <Link
               href="/new"
--- app/src/components/general/Balance.tsx
+++ app/src/components/general/Balance.tsx
@@ -11,9 +11,9 @@ export const Balance = () => {
 
   return (
     <div className="flex space-x-2 items-center">
-      <div className="flex space-x-1 items-center h-10 border-[1px] border-custom-border rounded-[36px] px-[10px] bg-[inherit]">
+      <div className="flex space-x-2 items-center h-10 border-[1px] border-custom-border rounded-[36px] px-[10px] bg-[inherit]">
         <Image src="/general/sui.svg" alt="plus" width={10} height={10} />
-        <div className="flex space-x-1 text-white text-opacity-90 text-sm">
+        <div className="flex space-x-1 text-white text-opacity-90 text-sm font-semibold">
           <span>{formatSUIAmount(balance)} </span>
           <span className="hidden md:block">SUI</span>
         </div>
@@ -21,11 +21,11 @@ export const Balance = () => {
       <LoadingButton
         onClick={handleRequestSui}
         isLoading={isLoading}
-        className="flex space-x-0 md:space-x-3 items-center border-[1px] border-custom-border rounded-[36px] px-[10px] bg-[inherit] hover:bg-[#12BF77]"
-        spinnerClassName="w-5 h-5"
+        className="flex space-x-0 md:space-x-2 items-center border-[1px] border-custom-border rounded-[36px] px-[10px] bg-[inherit] hover:bg-[#12BF77]"
+        spinnerClassName="!w-5 !h-5"
       >
-        <div className="hidden md:block">Top up</div>
         <Image src="/general/plus.svg" alt="plus" width={20} height={20} />
+        <div className="hidden md:block font-semibold">Top up</div>
       </LoadingButton>
     </div>
   );
