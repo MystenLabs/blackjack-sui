dbd42447697c4f6ee7da2d0e3d0bea258667005b
atha <giannisj3@gmail.com>
2024-02-26 15:49:00 +0200

    Add balance check to display faucet button

--- app/src/components/general/Balance.tsx
+++ app/src/components/general/Balance.tsx
@@ -4,6 +4,7 @@ import { useRequestSui } from "@/hooks/useRequestSui";
 import { formatSUIAmount } from "@/helpers/formatSUIAmount";
 import Image from "next/image";
 import { useBalance } from "@/contexts/BalanceContext";
+import BigNumber from "bignumber.js";
 
 export const Balance = () => {
   const { isLoading, handleRequestSui } = useRequestSui();
@@ -18,15 +19,17 @@ export const Balance = () => {
           <span className="hidden md:block">SUI</span>
         </div>
       </div>
-      <LoadingButton
-        onClick={handleRequestSui}
-        isLoading={isLoading}
-        className="flex space-x-0 md:space-x-2 items-center border-[1px] border-custom-border rounded-[36px] px-[10px] bg-[inherit] hover:bg-[#12BF77]"
-        spinnerClassName="!w-5 !h-5"
-      >
-        <Image src="/general/plus.svg" alt="plus" width={20} height={20} />
-        <div className="hidden md:block font-semibold">Top up</div>
-      </LoadingButton>
+      {balance <= BigNumber(0.5) && (
+        <LoadingButton
+          onClick={handleRequestSui}
+          isLoading={isLoading}
+          className="flex space-x-0 md:space-x-2 items-center border-[1px] border-custom-border rounded-[36px] px-[10px] bg-[inherit] hover:bg-[#12BF77]"
+          spinnerClassName="!w-5 !h-5"
+        >
+          <Image src="/general/plus.svg" alt="plus" width={20} height={20} />
+          <div className="hidden md:block font-semibold">Top up</div>
+        </LoadingButton>
+      )}
     </div>
   );
 };
