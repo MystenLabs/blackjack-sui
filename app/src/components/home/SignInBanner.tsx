16f71fef534c3e68acc01f61d40ad13612bcf129
alex <alex@mystenlabs.com>
2024-04-08 11:47:24 +0300

    Fix pre-basecamp breaking change to enoki

--- app/src/components/home/SignInBanner.tsx
+++ app/src/components/home/SignInBanner.tsx
@@ -25,6 +25,7 @@ export const SignInBanner = () => {
         provider: "google",
         clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
         redirectUrl,
+        network: "testnet",
         extraParams: {
           scope: ["openid", "email", "profile"],
         },
