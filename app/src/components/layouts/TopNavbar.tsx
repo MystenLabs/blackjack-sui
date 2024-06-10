9ca5f12aa9f0301ca7f32f287e5d126779cc0f4d
Sang Kim <nonvis@gmail.com>
2024-06-10 11:24:02 +0900

    Add a banner

--- app/src/components/layouts/TopNavbar.tsx
+++ app/src/components/layouts/TopNavbar.tsx
@@ -8,18 +8,26 @@ export const TopNavbar = () => {
   const { address } = useZkLogin();
 
   return (
-    <div className="backdrop-blur-md md:backdrop-blur-none sticky top-0 flex w-full h-full bg-inherit p-5 space-x-2 md:space-x-4 justify-between items-center z-10">
-      <Link href="/new" className="w-[min-content] md:w-[300px] text-2xl font-bold text-white">
-        Mysten Blackjack
-      </Link>
-      <div className="flex flex-1 justify-end items-center space-x-1">
-        {!!address && (
-          <div className="flex space-x-2 items-center">
-            <Balance />
-            <UserProfileMenu />
-          </div>
-        )}
+    <>
+      <div className={`fixed top-0 w-full flex justify-evenly z-30 transition-all`}>
+        <div className="mx-5 flex h-16 items-center justify-end text-white ">
+          <span>[Mysten Blackjack] is provided for testnet purposes only and do not involve real money or the opportunity to win real money.</span>
+        </div>
       </div>
-    </div>
+      <div
+        className="backdrop-blur-md md:backdrop-blur-none w-full h-full bg-inherit p-5 space-x-2 md:space-x-4 justify-between items-center z-10">
+        <Link href="/new" className="w-[min-content] md:w-[300px] text-2xl font-bold text-white">
+          Mysten Blackjack
+        </Link>
+        <div className="flex flex-1 justify-end items-center space-x-1">
+          {!!address && (
+            <div className="flex space-x-2 items-center">
+              <Balance/>
+              <UserProfileMenu/>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
