6dc79825dce3472518af7be238910b266a91a737
Milan Kyncl <milan.kyncl@icloud.com>
2025-08-21 11:49:15 +0200

    feat: removed unused and deprecated implementation of sponsored transaction

--- app/src/hooks/useSui.ts
+++ app/src/hooks/useSui.ts
@@ -1,40 +1,8 @@
-import {
-  SuiClient,
-  SuiTransactionBlockResponseOptions,
-} from "@mysten/sui/client";
-import { Transaction } from "@mysten/sui/transactions";
-import { useEnokiFlow } from "@mysten/enoki/react";
-
-interface EnokiSponsorExecuteProps {
-  transaction: Transaction;
-  options?: SuiTransactionBlockResponseOptions;
-}
+import { SuiClient } from "@mysten/sui/client";
 
 export const useSui = () => {
   const FULL_NODE = process.env.NEXT_PUBLIC_SUI_NETWORK!;
   const suiClient = new SuiClient({ url: FULL_NODE });
-  const enokiFlow = useEnokiFlow();
 
-  //TODO: Change approach here since this is deprecated
-  const enokiSponsorExecute = async ({
-    transaction,
-    options,
-  }: EnokiSponsorExecuteProps) => {
-    return enokiFlow
-      .sponsorAndExecuteTransaction({
-        network: process.env.NEXT_PUBLIC_SUI_NETWORK?.includes("testnet")
-          ? "testnet"
-          : "mainnet",
-        transaction: transaction as any,
-        client: suiClient as any,
-      })
-      .then((resp) => {
-        return suiClient.getTransactionBlock({
-          digest: resp.digest,
-          options,
-        });
-      });
-  };
-
-  return { enokiSponsorExecute, suiClient };
+  return { suiClient };
 };
