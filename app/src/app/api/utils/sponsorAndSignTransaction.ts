71e3d3a63175aae28fda6e8e1070d4f517758463
nikos-kitmeridis <145983019+nikos-kitmeridis@users.noreply.github.com>
2025-09-20 13:19:14 +0300

    fixes setup sponsorship (#59)
    * fixes setup scripts
    
    * fixes sponsorship

--- app/src/app/api/utils/sponsorAndSignTransaction.ts
+++ app/src/app/api/utils/sponsorAndSignTransaction.ts
@@ -4,7 +4,6 @@ import { getKeypair } from "../helpers/getKeyPair";
 import { enokiClient } from "@/app/api/EnokiClient";
 import serverConfig from "@/config/serverConfig";
 import { toBase64 } from "@mysten/sui/utils";
-import {getAddress} from "@/app/api/helpers/getAddress";
 import getMoveTarget from "@/helpers/getMoveTarget";
 
 interface SponsorAndSignTransactionProps {
@@ -25,8 +24,8 @@ export const sponsorAndSignTransaction = async ({
   const sponsoredTransaction = await enokiClient.createSponsoredTransaction({
     network: serverConfig.NEXT_PUBLIC_SUI_NETWORK_NAME,
     transactionKindBytes: toBase64(txBytes),
-    sender: getAddress(serverConfig.ADMIN_SECRET_KEY),
-    allowedAddresses: [getAddress(serverConfig.ADMIN_SECRET_KEY)],
+    sender: serverConfig.NEXT_PUBLIC_ADMIN_ADDRESS,
+    allowedAddresses: [serverConfig.NEXT_PUBLIC_ADMIN_ADDRESS],
     allowedMoveCallTargets: [
       // dealer interactions
       getMoveTarget('single_player_blackjack', 'first_deal'),
